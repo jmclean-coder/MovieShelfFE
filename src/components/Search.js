a91493c417beef7ae85317860435b4e16cabdbe8
Trevor <tlow94@uw.edu>
2020-07-06 16:26:08 -0700

    title-search-part-1

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search() {
+    return (
+       <section className="search-wrap">
+        <input type="text" placeHolder="Search for Movie Title" className="search"/>
+       </section>
+    )
+}
+
+export default Search
+
