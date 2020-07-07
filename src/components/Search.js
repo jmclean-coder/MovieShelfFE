829dcffa5ce89215c1c754bed0b5290385053504
Trevor <tlow94@uw.edu>
2020-07-07 10:14:15 -0700

    search-input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Search() {
+function Search(props) {
     return (
        <section className="search-wrap">
-        <input type="text" placeHolder="Search for Movie Title" className="search"/>
+        <input type="text" placeHolder="Search for Movie Title" className="search" onChange={props.handleInput}/>
        </section>
     )
 }
