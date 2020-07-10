152ae9eb703391d29cd9a32abd2dddc7eba44d6f
Joshua Mclean <joshmcleana@gmail.com>
2020-07-10 13:01:33 -0700

    home page imahe App.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../App.css"
 
 class Home extends React.Component{
 
@@ -8,6 +9,10 @@ class Home extends React.Component{
             <header>
                 <br></br>
             <h1>Welcome!</h1>
+
+            <div id="image-container">
+            <img src={'https://techcrunch.com/wp-content/uploads/2018/09/GettyImages-104187321.jpg'}></img>
+            </div>
             </header>
         )
     }
