b90ba17aba65d72c2ac78d6fce4dee88333092ad
Colton <kaiserc2@seattleu.edu>
2020-07-10 09:56:53 -0700

    Adds edit form for shelf cards

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,9 +4,19 @@ import Card from 'react-bootstrap/Card'
 
 
 class MovieCard extends Component {
+
+    state = {
+        image: ''
+    }
+
     handleClick = () => {
         this.props.deleteFromShelf(this.props.movie.imdb_id)
     }
+
+    handleEditClick = () => {
+        this.props.showForm()
+    }
+
     render() {
         return (
             <Fragment>
@@ -16,6 +26,7 @@ class MovieCard extends Component {
                     <Card.Title as="h4" className="text-center">{this.props.movie.title} - {this.props.movie.year}</Card.Title>
                     </Card.Body>
                     <Card.Img variant="top" src={this.props.movie.poster}/>
+                    <Button variant="info" onClick={this.handleEditClick}>Edit Poster</Button>
                     <Button variant="danger" onClick={this.handleClick}>Delete</Button>
                     </Card>
                 </div>
