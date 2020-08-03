01cea31943789ed0b57a06a38f7ab98f4340b8f9
Joshua Mclean <joshmcleana@gmail.com>
2020-08-03 12:39:27 -0700

    MErge chnages from ckaiser

--- src/containers/ShelfPage.js
+++ src/containers/ShelfPage.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import MovieCard from '../components/MovieCard'
-import CardGroup from 'react-bootstrap/CardGroup'
+import React from "react";
+import { Container, Jumbotron } from "react-bootstrap";
+import MovieCard from "../components/MovieCard";
+import CardGroup from "react-bootstrap/CardGroup";
 
-function ShelfPage(props) {
-        let rowCount = 1
-        return (
-            <div>
-                <h1 className="text-center">My Movies!</h1>
-                <Container>
-                <CardGroup>
-                    {props.myShelf.map(movie => (
-                        <MovieCard movie={movie} deleteFromShelf={props.deleteFromShelf} handleEditSubmit={props.handleEditSubmit} />
-                    ))}
-                </CardGroup>
-                </Container>
-            </div>
-        )
+const ShelfPage = (props) => {
+    console.log(props.myShelf)
+  return props.myShelf.length ? (
+    <div>
+      <h1 className="text-center">My Movies!</h1>
+      <Container>
+        <CardGroup>
+          {props.myShelf.map((movie) => (
+            <MovieCard
+              movie={movie}
+              deleteFromShelf={props.deleteFromShelf}
+              handleEditSubmit={props.handleEditSubmit}
+            />
+          ))}
+        </CardGroup>
+      </Container>
+    </div>
+  ) : (<h1 className="text-center" style={{marginTop: 300}}>You don't have any movies on your shelf yet</h1>
+  );
+};
 
-}
-
-export default ShelfPage                                                                                                                                                                                                                                                                                         
+export default ShelfPage;
