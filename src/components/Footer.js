d2720fec8ba87653f821f0c682fad796b67c7c98
Colton <kaiserc2@seattleu.edu>
2020-08-10 18:52:06 -0700

    Adds links to githubs in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from "react";
 import Container from "react-bootstrap/Container";
 
 export default function Footer() {
-    return (
-        <Container id="site-footer">
-            <h3 className="footer-text">Created by: Justin Langlinais, Colton Kaiser, Joshua Mclean, and Trevor Low</h3>
-        </Container>
-    )
+  return (
+    <Container id="site-footer">
+      <h3 className="footer-text">
+        Created by:{" "}
+        <a href="https://github.com/justinclanglinais" style={{ color: "white" }}>Justin Langlinais</a>,{" "}
+        <a href="https://github.com/ckaiser258" style={{ color: "white" }}>Colton Kaiser</a>,{" "}
+        <a href="https://github.com/jmclean-coder" style={{ color: "white" }}>Joshua Mclean</a>, and{" "}
+        <a href="https://github.com/MikaJakePizzaBake" style={{ color: "white" }}>Trevor Low</a>
+      </h3>
+    </Container>
+  );
 }
