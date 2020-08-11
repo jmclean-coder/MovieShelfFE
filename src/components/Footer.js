65fe6edb815f4170f051e7ac406df956dff65862
Colton <kaiserc2@seattleu.edu>
2020-08-10 18:56:36 -0700

    Removes underline from nav bar links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,37 @@ export default function Footer() {
     <Container id="site-footer">
       <h3 className="footer-text">
         Created by:{" "}
-        <a href="https://github.com/justinclanglinais" style={{ color: "white" }}>Justin Langlinais</a>,{" "}
-        <a href="https://github.com/ckaiser258" style={{ color: "white" }}>Colton Kaiser</a>,{" "}
-        <a href="https://github.com/jmclean-coder" style={{ color: "white" }}>Joshua Mclean</a>, and{" "}
-        <a href="https://github.com/MikaJakePizzaBake" style={{ color: "white" }}>Trevor Low</a>
+        <a
+          href="https://github.com/justinclanglinais"
+          style={{ color: "white", textDecoration: "underline" }}
+        >
+          Justin Langlinais
+        </a>
+        ,{" "}
+        <a
+          href="https://github.com/ckaiser258"
+          style={{ color: "white", textDecoration: "underline" }}
+        >
+          Colton Kaiser
+        </a>
+        ,{" "}
+        <a
+          href="https://github.com/jmclean-coder"
+          style={{ color: "white", textDecoration: "underline" }}
+        >
+          Joshua Mclean
+        </a>
+        , and{" "}
+        <a
+          href="https://github.com/MikaJakePizzaBake"
+          style={{ color: "white", textDecoration: "underline" }}
+        >
+          Trevor Low
+        </a>
       </h3>
+      {
+        //Ran into trouble trying to style these in the css file or in the h2/container tags. So styles are declared inline here
+      }
     </Container>
   );
 }
