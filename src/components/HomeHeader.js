90af4ec6822620f729f689014545c29533a98c7b
Joshua Mclean <joshmcleana@gmail.com>
2020-08-10 21:22:02 -0700

    Resize and style Image on HomePage

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 export default function HomeHeader() {
     return (
         <Container className="page-header-box"  >
-        {/* <h1 className="page-header">Welcome to MovieShelf</h1> */}
+        <h1 className="page-header">Welcome to MovieShelf</h1>
       </Container>
     )
 }
