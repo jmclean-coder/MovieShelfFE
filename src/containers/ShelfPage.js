dd069703a01014d44d91c13b4db6f88778a57dae
Colton <kaiserc2@seattleu.edu>
2020-08-10 19:24:33 -0700

    Removes ShelfHeader as it's own component as it wasn't formatting properly

--- src/containers/ShelfPage.js
+++ src/containers/ShelfPage.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useContext} from "react";
+import {FilterContext} from "../App"
 import { Container } from "react-bootstrap";
 import MovieCard from "../components/MovieCard";
 import CardGroup from "react-bootstrap/CardGroup";
 
 const ShelfPage = (props) => {
-  console.log(props.myShelf);
+  console.log(props.myShelf)
+  console.log(FilterContext.currentGenre);
   return (
     <>
+    <Container className="page-header-box">
+    {props.myShelf.length ? (
+      <h1 className="page-header">Nice shelf, you have good taste.</h1>
+    ) : (
+      <h1 className="page-header">
+        Your shelf is empty!
+      </h1>
+    )}
+  </Container>
       <Container 
       className="content-wrapper"
      // style={{ marginTop: "105px" }}
