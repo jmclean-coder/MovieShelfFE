4a99a6f3c35ca3732b29b8e559e7c73691776dd9
Joshua Mclean <joshmcleana@gmail.com>
2020-08-13 17:03:20 -0700

    Remove abstracted shelf header

--- src/containers/ShelfPage.js
+++ src/containers/ShelfPage.js
@@ -8,13 +8,6 @@ const ShelfPage = (props) => {
   const filterContext = useContext(FilterContext);
   return (
     <>
-      <Container className="page-header-box">
-        {props.myShelf.length ? (
-          <h1 className="page-header">Nice shelf, you have good taste.</h1>
-        ) : (
-          <h1 className="page-header">Your shelf is empty!</h1>
-        )}
-      </Container>
       <Container
         className="content-wrapper"
         // style={{ marginTop: "105px" }}
